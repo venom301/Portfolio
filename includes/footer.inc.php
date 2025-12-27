4de1fe50db8431ea53f6245eb7ffd5d851b61e83
felix <ifyifeanyi301@gmail.com>
2025-12-27 12:10:15 +0100

    converted php portfolio to js portfolio static page

--- includes/footer.inc.php
+++
@@ -1,5 +0,0 @@
-<footer class="bg-gray-900 text-gray-400 py-8 px-4 sm:px-6">
-    <div class="max-w-6xl mx-auto text-center">
-        <p>&copy; 2025 Felix Portfolio. Crafted with care.</p>
-    </div>
-</footer>
--- start-server.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting PHP development server...
-cd %~dp0
-php -S localhost:8000 -t public
-pause
